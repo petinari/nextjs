1e2965acc65b3e6daa80f2158320dec5b6ee1e8f
robson <petinari@outlook.com>
2022-11-22 08:48:55 -0400

    1

--- components/InputField/index.tsx
+++ components/InputField/index.tsx
@@ -1,47 +1,53 @@
-
-import { useState } from "react"
-import styles from "./styles.module.css"
-import VectorSenha from "./VectorSenha.svg"
-import VectorSenha1 from "./VectorSenha1.svg"
+import { useState } from "react";
+import styles from "./styles.module.css";
+import VectorSenha from "./VectorSenha.svg";
+import VectorSenha1 from "./VectorSenha1.svg";
 
 type Props = {
-    color: string
-    placeholder: string
-    value: string
-    onChange: (newValue: string) => void
-    password?: boolean
-}
+  color: string;
+  placeholder: string;
+  value: string;
+  onChange: (newValue: string) => void;
+  password?: boolean;
+};
 
-export const InputField = ({color, placeholder, value, onChange, password}: Props) => {
+export const InputField = ({
+  color,
+  placeholder,
+  value,
+  onChange,
+  password,
+}: Props) => {
+  const [showPassword, setShowPassword] = useState(false);
+  const [focused, setFocused] = useState(false);
+  const toggleShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
 
-    const [showPassword, setShowPassword] = useState(false);
-    const [focused, setFocused] = useState(false);
-    const toggleShowPassword = () =>{
-        setShowPassword(!showPassword)
-    }
+  return (
+    <div
+      className={styles.container}
+      style={{
+        borderColor: focused ? color : "#fbfbf9",
+        backgroundColor: focused ? "#FFF" : "#f9f9fb",
+      }}
+    >
+      <input
+        type={password ? (showPassword ? "text" : "password") : "text"}
+        className={styles.input}
+        placeholder={placeholder}
+        value={value}
+        onChange={(e) => onChange(e.target.value)}
+        onFocus={() => setFocused(true)}
+        onBlur={() => setFocused(false)}
+      />
 
-    return(
-        <div className={styles.container}
-            style={{borderColor: focused ? color : '#fbfbf9', backgroundColor: focused ? '#FFF' : '#f9f9fb'}}>
-            <input type={password ? (showPassword ? 'text' : 'password'): 'text'} 
-            className={styles.input}
-            placeholder={placeholder}
-            value={value}
-            onChange={e => onChange(e.target.value)}
-            onFocus={() => setFocused(true)}
-            onBlur={() => setFocused(false)}/>
-            
-            {
-                password &&
-                <div className={styles.showPassword}
-                    onClick={toggleShowPassword}    >
-                
-               {showPassword && <VectorSenha color="grey"/>}
-               {!showPassword && <VectorSenha1 color="grey"/>}
-            </div>
-            }       
-                
+      {password && (
+        <div className={styles.showPassword} onClick={toggleShowPassword}>
+          {showPassword && <VectorSenha color="grey" />}
+          {!showPassword && <VectorSenha1 color="grey" />}
         </div>
-    );      
-    
-}
+      )}
+    </div>
+  );
+};
--- pages/[tenant]/login.tsx
+++ pages/[tenant]/login.tsx
@@ -1,47 +1,57 @@
 import { GetServerSideProps } from "next";
 import Head from "next/head";
 import { useEffect, useState } from "react";
+import { Button } from "../../components/Button";
 import { Header } from "../../components/Header";
 import { InputField } from "../../components/InputField";
-
-
 import { useAppContext } from "../../contexts/AppContext";
-import {  useApi } from "../../libs/useAPI";
-
-import styles from "../../styles/login.module.css"
+import { useApi } from "../../libs/useAPI";
+import styles from "../../styles/login.module.css";
 import { Tenant } from "../../types/Tenant";
 
 const Login = (data: Props) => {
-
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const {tenant, setTenant} = useAppContext();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { tenant, setTenant } = useAppContext();
   const loginText = `Login ${data.tenant.name}`;
-  useEffect(()=> {setTenant(data.tenant), []})
+  useEffect(() => {
+    setTenant(data.tenant), [];
+  });
+  const handleSubmit = () => {};
 
   return (
     <div className={styles.container}>
-        <Head>
-            <title>{loginText}</title>
-        </Head>
-        <Header color={data.tenant.mainColor} backHref={`/${data.tenant.slug}`}/>
-          
-        <InputField
-          color={data.tenant.mainColor}
-          
-          placeholder="Digite seu e-mail"
-          value={email}
-          onChange={setEmail}/>
-        
-         <InputField
-          color={data.tenant.mainColor}
-          
-          placeholder="Digite sua senha"
-          value={password}
-          onChange={setPassword}
-          password/>
-       
-       
+      <Head>
+        <title>{loginText}</title>
+      </Head>
+      <Header color={data.tenant.mainColor} backHref={`/${data.tenant.slug}`} />
+
+      <InputField
+        color={data.tenant.mainColor}
+        placeholder="Digite seu e-mail"
+        value={email}
+        onChange={setEmail}
+      />
+
+      <InputField
+        color={data.tenant.mainColor}
+        placeholder="Digite sua senha"
+        value={password}
+        onChange={setPassword}
+        password
+      />
+
+      <Button
+        color={data.tenant.mainColor}
+        label="Entrar"
+        onClick={handleSubmit}
+        fill
+      />
+      <Button
+        color={data.tenant.mainColor}
+        label="Entrar"
+        onClick={handleSubmit}
+      />
     </div>
   );
 };
